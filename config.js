18623224e90330177435a71b9385b1ddd0d5c24b
nodirbek-py <nodvos@gmail.com>
2021-07-07 16:01:06 +0500

    almost ready

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = { 
+    token: "1853795211:AAHhuGQAlFhnRc1U7kaP8LwK86ShUc1OqtU",
+    salom_uz: "Assalamu Alaykum",
+    fakultet: "Fakultetlar",
+    fakultet_rus: "Факультеты",
+    royxat:"Kerakli bo'ladigan hujjatlar ro'yxati",
+    royxat_rus:"Список необходимых документов"
+};
+
+// ctx.reply(
+//     `
+//     Yo'nalishni tanlang
+// 👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+//     `,
+//     Markup.inlineKeyboard([
+//       [Markup.callbackButton("👨‍💻 Texnologiya bakalavr darajasi", "tech")],
+//       [
+//         Markup.callbackButton(
+//           "💼 Biznes boshqaruvi bakalavr darajasi",
+//           "business"
+//         ),
+//       ],
+//       [
+//         Markup.callbackButton(
+//           "💻 Kompyuter ilovalari bo'yicha aspirantura diplomi",
+//           "comp"
+//         ),
+//       ],
+//       [
+//         Markup.callbackButton(
+//           "💪 Malaka rivojlantirishni sertifikatlash kursi",
+//           "skill"
+//         ),
+//       ],
+//       [Markup.callbackButton("🇬🇧 Ingliz tili amaliy kursi ", "eng_prac")],
+//       [Markup.callbackButton("🇬🇧 Ingliz tili tayyorlov kursi", "eng_prep")],
+//     ]).extra()
+//   );
